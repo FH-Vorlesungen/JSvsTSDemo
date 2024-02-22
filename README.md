fcf0208c9a8cd887e1cb093f5e58e49fffa30d67
FSchmuck <florian.schmuck@bearingpoint.com>
2024-02-22 10:08:23 +0100

    Demo setup for TS to JS conversion

---
+++ README.md
@@ -0,0 +1,4 @@
+1. npm init
+2. npm i typescript --save-dev
+3. npx tsc init
+4. npx tsc index.ts
---
+++ index.ts
@@ -0,0 +1,6 @@
+const x = 1
+document.writeln(x.toString());
+const y = x+'1'
+document.writeln(y);
+const z = y-1;
+document.writeln(z);
